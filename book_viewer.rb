127c28ddbe9108bf30effad1bdef3601233bdbc9
Clay Karwisch <clay.karwisch@gmail.com>
2020-11-10 09:50:39 -0500

    add redirects and view helper

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,23 +2,38 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+before do
+  @table_of_contents = File.readlines('data/toc.txt')
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @table_of_contents = File.readlines('data/toc.txt')
 
   erb :home
 end
 
 get "/chapters/:number" do
-  number = params[:number]
+  number = params[:number].to_i
+  redirect "/" unless (1..@table_of_contents.size).include?(number)
 
-  @table_of_contents = File.readlines('data/toc.txt')
-  @title = "Chapter #{number}: #{@table_of_contents[number.to_i - 1]}"
-  @text = File.read("data/chp#{number}.txt")
+  @title = "Chapter #{number}: #{@table_of_contents[number - 1]}"
+  @text = File.read("data/chp#{number.to_s}.txt")
 
   erb :chapter
 end
 
+helpers do
+  def in_paragraphs(text)
+    @text.split("\n\n").map do |paragraph|
+      "<p>#{paragraph}</p>"
+    end.join
+  end
+end
+
 get "/show/:name" do
   params[:name]
+end
+
+not_found do
+  redirect "/"
 end
