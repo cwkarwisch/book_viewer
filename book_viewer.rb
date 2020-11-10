0d10614ba4ff060d11d4e6ec942a4c96c48da9c0
Clay Karwisch <clay.karwisch@gmail.com>
2020-11-09 20:36:39 -0500

    add view templates

--- book_viewer.rb
+++ book_viewer.rb
@@ -1,6 +1,18 @@
+require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-  File.read "public/template.html"
+  @title = "The Adventures of Sherlock Holmes"
+  @table_of_contents = File.readlines('data/toc.txt')
+
+  erb :home
+end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @table_of_contents = File.readlines('data/toc.txt')
+  @text = File.read('data/chp1.txt')
+
+  erb :chapter
 end
