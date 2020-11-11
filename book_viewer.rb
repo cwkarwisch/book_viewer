3b4f36331bfbc260c01fd3f98c5a2ae43ea8d07a
Clay Karwisch <clay.karwisch@gmail.com>
2020-11-11 11:21:49 -0500

    refactor search functionality

--- book_viewer.rb
+++ book_viewer.rb
@@ -23,58 +23,35 @@ get "/chapters/:number" do
 end
 
 get "/search" do
-  @results = search_results(params) unless !params['query']
+  @results = find_matching_chapters(params['query']) unless !params['query']
   erb :search
 end
 
 helpers do
   def in_paragraphs(text)
-    index = -1
-    @text.split("\n\n").map do |paragraph|
-      index += 1
-      "<p id='#{index}'>#{paragraph}</p>"
+    @text.split("\n\n").each_with_index.map do |paragraph, index|
+      "<p id='paragraph#{index}'>#{paragraph}</p>"
     end.join
   end
 
-  def extract_chapter_number(chapter_file_name)
-    match = chapter_file_name.match(/[0-9]+/)
-    if match
-      match[0]
-    else
-      nil
+  def each_chapter
+    @table_of_contents.each_with_index do |chapter_file_name, chapter_number|
+      chapter_number += 1
+      text = File.read("data/chp#{chapter_number}.txt")
+      yield chapter_file_name, chapter_number, text if block_given?
     end
-  end
-
-  def extract_chapter_name(chapter_file_name)
-    number = extract_chapter_number(chapter_file_name).to_i
-    @table_of_contents[number - 1]
   end
 
   def find_matching_chapters(query)
     results = []
-    Dir.entries("data").each do |chapter|
-      next if File.directory?(chapter)
-      chapter_hash = { name: extract_chapter_name(chapter),
-      number: extract_chapter_number(chapter),
-      paragraphs: [] }
-      match_found = false
-      text = File.read("data/#{chapter}")
+    each_chapter do |name, number, text|
+      matching_paragraphs = {}
       text.split("\n\n").each_with_index do |paragraph, index|
-        if paragraph.include?(query)
-          chapter_hash[:paragraphs] << { text: paragraph,
-                                         index: index }
-          results << chapter_hash unless match_found == true
-          match_found = true
-        end
+        matching_paragraphs[index] = paragraph if paragraph.include?(query)
       end
+      results << { name: name, number: number, paragraphs: matching_paragraphs } if matching_paragraphs.any?
     end
     results
-  end
-
-  def search_results(params)
-    results = []
-    query = params['query']
-    find_matching_chapters(query)
   end
 
   def bold_phrase(text, phrase)
