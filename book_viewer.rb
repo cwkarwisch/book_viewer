8394985df9572691490eb9e6bb36730f09a5a8a1
Clay Karwisch <clay.karwisch@gmail.com>
2020-11-11 09:57:12 -0500

    update search results to show matching paragraphs

--- book_viewer.rb
+++ book_viewer.rb
@@ -23,30 +23,64 @@ get "/chapters/:number" do
 end
 
 get "/search" do
-  return erb :search if params['query'] == nil
-  @query = params['query']
-  @chapters = Dir.entries("data").select do |chapter|
-    next if File.directory?(chapter)
-    text = File.read("data/#{chapter}")
-    text.include?(@query)
-  end
+  @results = search_results(params) unless !params['query']
   erb :search
 end
 
 helpers do
   def in_paragraphs(text)
+    index = -1
     @text.split("\n\n").map do |paragraph|
-      "<p>#{paragraph}</p>"
+      index += 1
+      "<p id='#{index}'>#{paragraph}</p>"
     end.join
   end
 
   def extract_chapter_number(chapter_file_name)
-    chapter_file_name.match(/[0-9]+/)[0]
+    match = chapter_file_name.match(/[0-9]+/)
+    if match
+      match[0]
+    else
+      nil
+    end
   end
-end
 
-get "/show/:name" do
-  params[:name]
+  def extract_chapter_name(chapter_file_name)
+    number = extract_chapter_number(chapter_file_name).to_i
+    @table_of_contents[number - 1]
+  end
+
+  def find_matching_chapters(query)
+    results = []
+    Dir.entries("data").each do |chapter|
+      next if File.directory?(chapter)
+      chapter_hash = { name: extract_chapter_name(chapter),
+      number: extract_chapter_number(chapter),
+      paragraphs: [] }
+      match_found = false
+      text = File.read("data/#{chapter}")
+      text.split("\n\n").each_with_index do |paragraph, index|
+        if paragraph.include?(query)
+          chapter_hash[:paragraphs] << { text: paragraph,
+                                         index: index }
+          results << chapter_hash unless match_found == true
+          match_found = true
+        end
+      end
+    end
+    results
+  end
+
+  def search_results(params)
+    results = []
+    query = params['query']
+    find_matching_chapters(query)
+  end
+
+  def bold_phrase(text, phrase)
+    replacement = "<strong>" + phrase + "</strong>"
+    text.gsub(phrase, replacement)
+  end
 end
 
 not_found do
